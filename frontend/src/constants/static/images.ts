418a25af7eabe896ef91162264eab25bc5fab75e
Sanjeev <sanjeev.kumar@7edge.com>
2025-12-19 12:46:27 +0530

    feat: add SVG icons for UI components
    Signed-off-by: Sanjeev <sanjeev.kumar@7edge.com>

---
+++ frontend/src/constants/static/images.ts
@@ -0,0 +1,28 @@
+const iconsDirectory = "/icons";
+export const icons = {
+  assignmentRequest: `${iconsDirectory}/assignment_request.svg`,
+  dashboard: `${iconsDirectory}/dashboard.svg`,
+  info: `${iconsDirectory}/info.svg`,
+  logout: `${iconsDirectory}/logout.svg`,
+  notifications: `${iconsDirectory}/notifications.svg`,
+  orders: `${iconsDirectory}/orders.svg`,
+  photoPlaceholder: `${iconsDirectory}/photo_placeholder.svg`,
+  products: `${iconsDirectory}/products.svg`,
+  search: `${iconsDirectory}/search.svg`,
+  settings: `${iconsDirectory}/settings.svg`,
+  successCheck: `${iconsDirectory}/success_check.svg`,
+  vendorAssigned: `${iconsDirectory}/vendor_assigned.svg`,
+  vendors: `${iconsDirectory}/vendors.svg`,
+};
+
+const illustrationsDirectory = "/illustrations";
+export const illustrations = {
+  assignmentRequestEmpty: `${illustrationsDirectory}/assignment_request_empty.svg`,
+  productsEmpty: `${illustrationsDirectory}/products_empty.svg`,
+  vendorAssignedEmpty: `${illustrationsDirectory}/vendor_assigned_empty.svg`,
+};
+
+const logosDirectory = "/logo";
+export const logos = {
+  app_logo_main: `${logosDirectory}/app_logo_main.svg`,
+};
